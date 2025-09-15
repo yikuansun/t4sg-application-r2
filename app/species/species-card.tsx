2c4a216e8a8dba87af6fa791517bb91894c70f52
yikuansun <yikuansun2015@gmail.com>
2025-09-15 17:13:55 -0400

    Create dialogOpen state

--- app/species/species-card.tsx
+++ app/species/species-card.tsx
@@ -13,9 +13,13 @@ can cause errors with matching props and state in child components if the list o
 import { Button } from "@/components/ui/button";
 import type { Database } from "@/lib/schema";
 import Image from "next/image";
+import { useState } from "react";
 type Species = Database["public"]["Tables"]["species"]["Row"];
 
 export default function SpeciesCard({ species }: { species: Species }) {
+  // We use dialogOpen state to control the display of the dialog.
+  const [dialogOpen, setDialogOpen] = useState(false);
+
   return (
     <div className="m-4 w-72 min-w-72 flex-none rounded border-2 p-3 shadow">
       {species.image && (
@@ -27,7 +31,16 @@ export default function SpeciesCard({ species }: { species: Species }) {
       <h4 className="text-lg font-light italic">{species.common_name}</h4>
       <p>{species.description ? species.description.slice(0, 150).trim() + "..." : ""}</p>
       {/* Replace the button with the detailed view dialog. */}
-      <Button className="mt-3 w-full">Learn More</Button>
+      <Button
+        className="mt-3 w-full"
+        onClick={() => {
+          setDialogOpen(!dialogOpen);
+        }}
+      >
+        Learn More
+      </Button>
+
+      {dialogOpen ? <div>Dialog</div> : null}
     </div>
   );
 }
